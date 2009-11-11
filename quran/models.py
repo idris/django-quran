a91474980e5a499ce2da81d805d6e56c6bc609ea
idris <idris.m@gmail.com>
2009-11-11 01:35:12 -0500

    setup models for Sura and Aya

--- quran/models.py
+++ quran/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Sura(models.Model):
+    """Sura (chapter) of the Quran"""
+
+    REVELATION_CHOICES = (
+        ('Meccan', 'Meccan'),
+        ('Medinan', 'Medinan'),
+    )
+
+    index = models.IntegerField(primary_key=True, verbose_name='Sura Number')
+    name = models.CharField(max_length=50, verbose_name='Sura Name')
+    tname = models.CharField(max_length=50, verbose_name='English Transliterated Name')
+    ename = models.CharField(max_length=50, verbose_name='English Name')
+    order = models.IntegerField(verbose_name='Revelation Order')
+    type = models.CharField(max_length=7, choices=REVELATION_CHOICES, verbose_name='')
+    rukus = models.IntegerField(verbose_name='Number of Rukus')
+    bismillah = models.CharField(max_length=50, blank=True, verbose_name='Bismillah')
+
+    def __unicode__(self):
+        return self.name
+
+
+class Aya(models.Model):
+    """Aya (verse) of the Quran"""
+
+    index = models.IntegerField(verbose_name='Aya Number')
+    sura = models.ForeignKey(Sura)
+    text = models.TextField()
+
+    def __unicode__(self):
+        return self.text
