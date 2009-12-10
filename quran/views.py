3894f1574c54d58d4e1b5b9860321a4c77002351
idris <idris.m@gmail.com>
2009-12-10 16:24:00 -0500

    add translations to aya and sura views

--- quran/views.py
+++ quran/views.py
@@ -5,22 +5,20 @@ def index(request, template_name='quran/index.html'):
     suras = get_list_or_404(Sura)
     return render_to_response(template_name, {'suras': suras})
 
-def sura(request, sura_number, template_name='quran/sura.html'):
-    try:
-        sura = get_object_or_404(Sura, number=sura_number)
-        ayas = sura.ayas.all()
-    except Sura.DoesNotExist:
-        raise Http404
+def sura(request, sura_number, translation=1, template_name='quran/sura.html'):
+    sura = get_object_or_404(Sura, number=sura_number)
+    ayas = sura.ayas.all()
+    if translation:
+        translations = sura.translations.filter(translation=translation)
+        ayas = zip(ayas, translations)
     return render_to_response(template_name, {'sura': sura, 'ayas': ayas})
 
-def aya(request, sura_number, aya_number, template_name='quran/aya.html'):
-    try:
-        sura = get_object_or_404(Sura, number=sura_number)
-        aya = get_object_or_404(Aya, sura=sura, number=aya_number)
-        words = aya.words.all()
-    except Sura.DoesNotExist, Aya.DoesNotExist:
-        raise Http404
-    return render_to_response(template_name, {'sura': sura, 'aya': aya, 'words': words})
+def aya(request, sura_number, aya_number, translation=1, template_name='quran/aya.html'):
+    sura = get_object_or_404(Sura, number=sura_number)
+    aya = get_object_or_404(Aya, sura=sura, number=aya_number)
+    translated_aya = get_object_or_404(TranslatedAya, aya=aya, translation=translation)
+    words = aya.words.all()
+    return render_to_response(template_name, {'sura': sura, 'aya': aya, 'translation': translated_aya, 'words': words})
 
 def word(request, sura_number, aya_number, word_number, template_name='quran/word.html'):
     aya = get_object_or_404(Aya, sura=sura_number, number=aya_number)
